29a2023b3bdb564ae10d0236b9be47f2fe9b0fb1
anuj upadhyay <anuju760@gmail.com>
2023-09-26 21:11:38 +0530

    react based tic_tac_toe

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,101 @@
 import { useState } from 'react'
 
 import './App.css'
+function Square({value,onSquareClick}){
+  return (
+    <button className='square ' onClick={onSquareClick} >{value}</button>
+  );
 
-function App() {
-  const [count,setCount] = useState(0)
-  function handleClick(){
-    return setCount(count+1)
-  }
-  function reset(){
-    return setCount(0)
-  }
+
+  
+}
+
+
+
+ export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true)
+  const [squares,setSquares]=useState(Array(9).fill(null));
+
+  function handleClick(i){
+  ;//
+    //this method is about changing the data without mutating the data in the actual array here we create a copy of array by using .slice( )method   //const squares = [null, null, null, null, null, null, null, null, null];
+    //const nextSquares = ['X', null, null, null, null, null, null, null, null];
+    // Now `squares` is unchanged, but `nextSquares` first element is 'X' rather than `null`
+    
+      if(squares[i] || calculateWinner(squares)){
+        return;
+      }
+    
+    const nextSquares = squares.slice()
+    if (xIsNext){
+      nextSquares[i]= 'X';
+    }else{
+      nextSquares[i] = 'O'
+    }
+   
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext)
+    }
+
+    const winner =calculateWinner(squares);
+    let status;
+    if(winner){
+      status = 'Winner' + winner
+    }else{
+      status = 'Next Player' + (xIsNext ? 'X':'O')
+    }
 
   return (
     <>
-     <div className="wrapper">
-     <button onClick={handleClick} id='btn-1'>Increase  {count}</button>
-      <button onClick={reset} id='btn-2'>reset </button>
-     </div>
+    <div className='wrapper'>
+  <div className="board-row">
+   <Square value={squares[0]} onSquareClick={()=>handleClick(0)}/> 
+   <Square value={squares[1]} onSquareClick={()=>handleClick(1)}/>
+   <Square value={squares[2]} onSquareClick={()=>handleClick(2)}/>
 
-    </>
-  )
+   </div>
+
+  <div className="board-row">
+   <Square value={squares[3]}onSquareClick={()=>handleClick(3)} />
+   <Square value={squares[4]} onSquareClick={()=>handleClick(4)}/>
+   <Square value={squares[5]} onSquareClick={()=>handleClick(5)}/>
+
+
+  </div>
+  <div className="board-row">
+   <Square value={squares[6]} onSquareClick={()=>handleClick(6)}/>
+   <Square value={squares[7]} onSquareClick={()=>handleClick(7)}/>
+   <Square value={squares[8]} onSquareClick={()=> handleClick(8)}/>
+
+
+    
+  </div>
+  </div>
+  
+  
+  
+  
+  </>
+)
+
+
 }
 
-export default App
+function calculateWinner(squares){
+  const lines= [
+    [0,1,2],[3,4,5],[6,7,8],
+    [0,3,6],[1,4,7],[2,5,8],
+    [0,4,8],[2,4,6],
+  ];
+
+  for (let i = 0;i < lines.length;i++){
+    const [a,b,c]= lines[i];
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+      return squares[a];
+  }
+  }
+return null;
+}
+
+
+
